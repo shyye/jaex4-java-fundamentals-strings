40bf739ad4b88d9e5ef4ee9f2c2c95e2c20b7b60
shyye <emma.art.dev@gmail.com>
2024-08-13 08:51:19 +0200

    Solve test cases.

--- src/main/java/com/booleanuk/core/Exercise.java
+++ src/main/java/com/booleanuk/core/Exercise.java
@@ -7,7 +7,7 @@ public class Exercise {
     // 1. The brokenUrl member above contains an invalid URL. There's a z instead of an s in the protocol (httpz instead of https).
     // Using the `replace` method on brokenUrl, set the fixedUrl member below to the correct value.
     // https://docs.oracle.com/en/java/javase/18/docs/api/java.base/java/lang/String.html#replace(char,char)
-    public String fixedUrl = "";
+    public String fixedUrl = brokenUrl.replace("httpz", "https");
 
 
     // Here's a documentation link for all string methods, use it to figure out how to complete the rest of these requirements:
@@ -16,26 +16,26 @@ public class Exercise {
 
     // 2. There are currently some upper case characters in the URL. Using an appropriate string method on the fixedUrl member above,
     // set the value of lowerCasedUrl.
-    public String lowerCasedUrl = "";
+    public String lowerCasedUrl = fixedUrl.toLowerCase();
 
 
     // 3. There is still white space on both ends of the URL! Use the appropriate string method to trim that white space
     // and set the value of the url member below
-    public String url = "";
+    public String url = lowerCasedUrl.strip();
 
 
     // 4. Using the appropriate string method on url, set the value of the protocol member below
-    public String protocol = "";
+    public String protocol = url.split(":")[0];
 
 
     // 5. Using the appropriate string method on url, set the value of the domain member below
-    public String domain = "";
+    public String domain = url.split("/")[2];
 
 
     // 6. Set the length member below to the length of the url member
-    public int length = 0;
+    public int length = url.length();
 
 
     // 7. Using concatenation and existing members, set the faqUrl member below to the faq page of the boolean website
-    public String faqUrl = "";
+    public String faqUrl = url.replace(url.split("/")[3], "faq");
 }
